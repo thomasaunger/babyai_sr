e8130ecd7cc2dc466c29bda078cbb4c14d42b5be
Thomas A. Unger <69393628+thomasaunger@users.noreply.github.com>
2020-08-28 02:42:42 +0200

    Enhanced setup file

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name     = "babyai_sr",
       version  = "1.0",
-      packages = find_packages()
+      license  = "BSD-3-Clause",
+      keywords = "reinforcement learning, language emergence, multi-agent systems, language grounding, unsupervised semantics, deep learning, transfer learning",
+      packages = find_packages(),
+      install_requires = [
+                          "matplotlib>=3.3.1"
+                         ]
      )
