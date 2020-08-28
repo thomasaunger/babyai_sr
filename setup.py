f58e2339aece174e59d05288131cfc429f4e2e4b
Thomas A. Unger <69393628+thomasaunger@users.noreply.github.com>
2020-08-28 18:36:10 +0200

    Added curve plotting script

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name     = "babyai_sr",
       keywords = "reinforcement learning, language emergence, multi-agent systems, language grounding, unsupervised semantics, deep learning, transfer learning",
       packages = find_packages(),
       install_requires = [
-                          "matplotlib>=3.3.1"
+                          "matplotlib<=3.1.3"
                          ]
      )
