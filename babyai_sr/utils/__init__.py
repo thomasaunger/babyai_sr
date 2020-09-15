aed3b25aa60062cffd457fcc4a4b6ec950fced66
Thomas A. Unger <69393628+thomasaunger@users.noreply.github.com>
2020-09-15 22:55:04 +0200

    Save optimizers

---
+++ babyai_sr/utils/__init__.py
@@ -0,0 +1,1 @@
+from babyai_sr.utils.optimizer import load_optimizer, save_optimizer
---
+++ babyai_sr/utils/optimizer.py
@@ -0,0 +1,27 @@
+import os
+import torch
+
+import babyai.utils as utils
+
+
+def get_optimizer_path(model_name):
+    return os.path.join(utils.get_model_dir(model_name), "optimizer.pt")
+
+
+def load_optimizer(model_name, raise_not_found=True):
+    path = get_optimizer_path(model_name)
+    try:
+        if torch.cuda.is_available():
+            optimizer = torch.load(path)
+        else:
+            optimizer = torch.load(path, map_location=torch.device("cpu"))
+        return optimizer
+    except FileNotFoundError:
+        if raise_not_found:
+            raise FileNotFoundError("No optimizer found at {}".format(path))
+
+
+def save_optimizer(optimizer, model_name):
+    path = get_optimizer_path(model_name)
+    utils.create_folders_if_necessary(path)
+    torch.save(optimizer, path)
