fcb20beb2aea114fb4e89ffc177da446457f6bff
Thomas A. Unger <69393628+thomasaunger@users.noreply.github.com>
2020-10-02 00:04:47 +0200

    Simplification

--- babyai_sr/rl/utils/penv.py
+++ babyai_sr/rl/utils/penv.py
@@ -74,13 +74,16 @@ def worker(conn, env, n):
     while True:
         cmd, action, prev_result = conn.recv()
         if cmd == "step":
-            if prev_result[0]:
+            if prev_result[0][1]:
                 # receiver's frame
                 obs, reward, done, info = env.step(action)
                 done = done or 64 <= env.step_count
                 if done:
                     obs = env.reset()
-                active = env.step_count % n != 0
+                active_sender = env.step_count % n == 0
+                active_receiver = not active_sender
+                active = (active_sender, active_receiver)
+                sending = (active_sender, False)
                 globs = obs.copy()
                 globs["image"] = get_global(env, obs)
                 obs["image"]   = get_local(obs)
@@ -92,14 +95,20 @@ def worker(conn, env, n):
                 # sender's frame
                 reward = 0.0
                 done   = False
-                active = True
-                obs    = prev_result[2]
-                globs  = prev_result[1]
-                extra  = prev_result[3]
-            conn.send((active, globs, obs, extra, reward, done))
+                active_sender = False
+                active_receiver = not active_sender
+                active = (active_sender, active_receiver)
+                sending = (active_sender, False)
+                obs    = prev_result[3]
+                globs  = prev_result[2]
+                extra  = prev_result[4]
+            conn.send((active, sending, globs, obs, extra, reward, done))
         elif cmd == "reset":
             obs = env.reset()
-            active = env.step_count % n != 0
+            active_sender = env.step_count % n == 0
+            active_receiver = not active_sender
+            active = (active_sender, active_receiver)
+            sending = (active_sender, False)
             globs = obs.copy()
             globs["image"] = get_global(env, obs)
             obs["image"]   = get_local(obs)
@@ -107,7 +116,7 @@ def worker(conn, env, n):
             goal_type, goal_color = get_goal(env)
             goal_x, goal_y        = get_goal_loc(globs)
             extra = (agent_x, agent_y, goal_type, goal_color, goal_x, goal_y)
-            conn.send((active, globs, obs, extra))
+            conn.send((active, sending, globs, obs, extra))
         else:
             raise NotImplementedError
 
@@ -138,7 +147,10 @@ class ParallelEnv(gym.Env):
         for local in self.locals:
             local.send(("reset", None, None))
         obs = self.env[0].reset()
-        active = self.env[0].step_count % self.n != 0
+        active_sender = self.env[0].step_count % self.n == 0
+        active_receiver = not active_sender
+        active = (active_sender, active_receiver)
+        sending = (active_sender, False)
         globs = obs.copy()
         globs["image"]   = get_global(self.env[0], obs)
         obs["image"]     = get_local(obs)
@@ -146,19 +158,22 @@ class ParallelEnv(gym.Env):
         goal_type, goal_color = get_goal(self.env[0])
         goal_x, goal_y        = get_goal_loc(globs)
         extra = (agent_x, agent_y, goal_type, goal_color, goal_x, goal_y)
-        self.prev_results = [(active, globs, obs, extra)] + [local.recv() for local in self.locals]
+        self.prev_results = [(active, sending, globs, obs, extra)] + [local.recv() for local in self.locals]
         return zip(*self.prev_results)
 
     def step(self, actions):
         for local, action, prev_result in zip(self.locals, actions[1:, 1], self.prev_results[1:]):
             local.send(("step", action, prev_result))
-        if self.prev_results[0][0]:
+        if self.prev_results[0][0][1]:
             # receiver's frame
             obs, reward, done, info = self.env[0].step(actions[0, 1])
             done = done or 64 <= self.env[0].step_count
             if done:
                 obs = self.env[0].reset()
-            active = self.env[0].step_count % self.n != 0
+            active_sender = self.env[0].step_count % self.n == 0
+            active_receiver = not active_sender
+            active = (active_sender, active_receiver)
+            sending = (active_sender, False)
             globs = obs.copy()
             globs["image"]   = get_global(self.env[0], obs)
             obs["image"]     = get_local(obs)
@@ -170,11 +185,14 @@ class ParallelEnv(gym.Env):
             # sender's frame
             reward = 0.0
             done   = False
-            active = True
-            obs    = self.prev_results[0][2]
-            globs  = self.prev_results[0][1]
-            extra  = self.prev_results[0][3]
-        self.prev_results = [(active, globs, obs, extra, reward, done)] + [local.recv() for local in self.locals]
+            active_sender = False
+            active_receiver = not active_sender
+            active = (active_sender, active_receiver)
+            sending = (active_sender, False)
+            obs    = self.prev_results[0][3]
+            globs  = self.prev_results[0][2]
+            extra  = self.prev_results[0][4]
+        self.prev_results = [(active, sending, globs, obs, extra, reward, done)] + [local.recv() for local in self.locals]
         return zip(*self.prev_results)
 
     def render(self):
