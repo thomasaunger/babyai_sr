a736abe9607bb897eea9f2cd8e309a5ac19c09bb
Thomas A. Unger <69393628+thomasaunger@users.noreply.github.com>
2020-10-16 02:07:18 +0200

    Double logging

--- babyai_sr/utils/__init__.py
+++ babyai_sr/utils/__init__.py
@@ -1,2 +1,3 @@
 from babyai_sr.utils.format    import MultiObssPreprocessor
+from babyai_sr.utils.log       import configure_logging
 from babyai_sr.utils.optimizer import load_optimizer, save_optimizer
---
+++ babyai_sr/utils/log.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import logging
+
+from babyai import utils
+
+
+def get_log_dir(log_name):
+    return os.path.join(utils.storage_dir(), "logs", log_name)
+
+
+def get_log_path(log_name):
+    return os.path.join(get_log_dir(log_name), "log.log")
+
+
+def configure_logging(log_name, stream=False):
+    path = get_log_path(log_name)
+    utils.create_folders_if_necessary(path)
+    
+    logger = logging.getLogger(log_name)
+    logger.setLevel(logging.INFO)
+    
+    formatter = logging.Formatter("%(name)s: %(asctime)s: %(message)s")
+    
+    file_handler = logging.FileHandler(filename=path)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+    if stream:
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+    
+    return logger
